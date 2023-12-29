9440204922912fd7fc8ce50c7edd41a1c6972088
vysh1234vyshnav1223 <vyshnavnair378@gmail.com>
2023-12-29 12:38:47 +0530

    resolve code errors

--- app/src/components/LoadingIcon/LoadingIcon.jsx
+++ app/src/components/LoadingIcon/LoadingIcon.jsx
@@ -5,7 +5,7 @@ export default function LoadingIcon() {
     return (
         <div className='loadingIcon'>
             <video autoPlay loop muted width='100' height='100' className='loadingIcon-video'>
-                <source src={'http://localhost:4000/uploads/videos/loadingIcon.webm'} type='video/mp4' />
+                <source src={'https://wethepeople-project.onrender.com/uploads/videos/loadingIcon.webm'} type='video/mp4' />
                 Your browser does not support the video tag.
             </video>
         </div>
